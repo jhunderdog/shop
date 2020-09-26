3a3b0c77371905b0ffbf3d06ca811024145ec886
jhunderdog <33382water@gmail.com>
2020-09-26 11:24:32 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 import logo from "./logo.svg";
 import "./App.css";
 import data from "./data.js";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   let [shoes, shoes변경] = useState(data);
@@ -38,23 +39,45 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Jumbotron className="background">
-        <h1>20% Season Off</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </Jumbotron>
-      <div className="container">
-        <div className="row">
-          {shoes.map((item, j) => {
-            return <Item shoes={shoes[j]} j={j} />;
-          })}
+
+      <Route exact path="/">
+        <Jumbotron className="background">
+          <h1>20% Season Off</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Jumbotron>
+        <div className="container">
+          <div className="row">
+            {shoes.map((item, j) => {
+              return <Item shoes={shoes[j]} j={j} />;
+            })}
+          </div>
         </div>
-      </div>
+      </Route>
+
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src="https://codingapple1.github.io/shop/shoes1.jpg"
+                width="100%"
+              />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button>
+            </div>
+          </div>
+        </div>
+      </Route>
     </div>
   );
 }
