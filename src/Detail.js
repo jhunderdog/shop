ae2459d380ff9216b5056faa3e78a209bc091b94
jhunderdog <33382water@gmail.com>
2020-09-27 16:53:01 +0900

    sass

--- src/Detail.js
+++ src/Detail.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "./Detail.scss";
 
+let 박스 = styled.div`
+  padding: 20px;
+`;
+let 제목 = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.색상};
+`;
 function Detail(props) {
   let history = useHistory();
   let { id } = useParams();
-  let selected_item = props.shoes.find(function (item) {
-    return item.id == id;
-  });
+  let selected_item = props.shoes.find((item) => item.id == id);
   console.log(selected_item);
   return (
     <div className="container">
+      <박스>
+        <제목 className="red">상세페이지</제목>
+      </박스>
+      <div className="my-alert2">
+        <p>재고가 얼마 남지 않았습니다.</p>
+      </div>
       <div className="row">
         <div className="col-md-6">
           <img
